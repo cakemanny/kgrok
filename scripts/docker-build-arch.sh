f20f0e1201a48f5f08829c337be1a7be11653faa
cakemanny <goldingd89@gmail.com>
2025-07-04 21:37:15 +0200

    ci: docker: split image build over native runners

---
+++ scripts/docker-build-arch.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# This script is used in the docker image build workflow
+#
+# shellcheck disable=SC2001
+
+set -xeuo pipefail
+
+test -n "$GITHUB_REF"
+test -n "$GITHUB_REF_NAME"
+
+case "$GITHUB_REF" in
+    refs/tags/*)
+        tags[0]=${GITHUB_REF_NAME#v}
+        tags[1]="$(echo "${GITHUB_REF_NAME#v}" | sed 's/\.[0-9][0-9]*$//')"
+        tags[2]=latest
+        push=yes
+        ;;
+    refs/heads/*)
+        tags[0]=$GITHUB_REF_NAME
+        push=no
+        ;;
+    *)
+        exit 1;
+esac
+
+image=ghcr.io/cakemanny/kgrok-remote
+
+case $(uname -m) in
+    x86_64) suffix=amd64 ;;
+    aarch64|arm64) suffix=arm64 ;;
+    *) exit 1 ;;
+esac
+
+taglist=$(
+  for tag in "${tags[@]}"; do
+      echo -t "${image}:${tag}-${suffix}"
+  done
+)
+
+# shellcheck disable=SC2086
+docker build -f Dockerfile $taglist .
+
+if [[ $push != "yes" ]]; then
+    exit 0
+fi
+
+# shellcheck disable=SC2046
+docker push $(
+  for tag in "${tags[@]}"; do
+      printf "%s " "${image}:${tag}-${suffix}"
+  done
+)
---
+++ scripts/docker-create-manifest.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# shellcheck disable=SC2001
+
+set -xeuo pipefail
+
+test -n "$GITHUB_REF"
+test -n "$GITHUB_REF_NAME"
+
+case "$GITHUB_REF" in
+    refs/tags/*)
+        tags[0]=${GITHUB_REF_NAME#v}
+        tags[1]="$(echo "${GITHUB_REF_NAME#v}" | sed 's/\.[0-9][0-9]*$//')"
+        tags[2]=latest
+        ;;
+    refs/heads/*)
+        tags[0]=$GITHUB_REF_NAME
+        ;;
+    *)
+        exit 1;
+esac
+
+image=ghcr.io/cakemanny/kgrok-remote
+
+for tag in "${tags[@]}"; do
+    docker builder imagetools create -t "${image}:${tag}" \
+        "${image}:${tag}-amd64" \
+        "${image}:${tag}-arm64"
+    docker push "${image}:${tag}"
+done
